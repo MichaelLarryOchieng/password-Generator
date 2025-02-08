d1d58e6e939e0d91eb4cc5a2b13d854992f8f960
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2025-02-08 23:23:30 +0300

    Update passwordGenerator.py display digits

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -13,6 +13,7 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
     all_characters = letters + digits + symbols
     print(all_characters)
     print(letters)
+    print(digits)
 
     while True:
         password = ''
